c07d1e30c56efe921eaaa6fe0833a1ee7d25efb4
Philip Munksgaard <pmunksgaard@gmail.com>
2016-05-17 17:22:41 +0200

    Update chal2 and chal3 to use freqalyze and xorcrypt

--- chal2/src/main.rs
+++ chal2/src/main.rs
@@ -1,20 +1,12 @@
 extern crate hexstr;
+extern crate xorcrypt;
 
-fn xor(left: &[u8], right: &[u8]) -> Vec<u8> {
-    assert_eq!(left.len(), right.len());
-
-    left
-        .iter()
-        .zip(right.iter())
-        .map(|(&x, &y)| x ^ y)
-        .collect()
-}
 
 fn main() {
     let right = hexstr::parse(b"1c0111001f010100061a024b53535009181c");
     let left = hexstr::parse(b"686974207468652062756c6c277320657965");
 
-    let res = xor(&left, &right);
+    let res = xorcrypt::xor(&left, &right);
 
     let expected = hexstr::parse(b"746865206b696420646f6e277420706c6179");
 
--- chal3/src/main.rs
+++ chal3/src/main.rs
@@ -6,17 +6,9 @@ extern crate freqalyze;
 fn main() {
     let s = hexstr::parse(b"1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
 
-    let mut v: Vec<(Vec<u8>, u64)> = Vec::new();
+    let key = freqalyze::find_key(&s);
 
-    for i in 0...255 {
-        let tmp: Vec<u8> = s.iter().map(|x| { x ^ i }).collect();
-        let score = freqalyze::score(&tmp);
-        v.push((tmp, score));
-    }
+    let decoded: Vec<u8> = s.iter().map(|x| x ^ key).collect();
 
-    v.sort_by(|&(_, a), &(_, b)| {b.cmp(&a)});
-    let &(ref string, score) = v.first().unwrap();
-    println!("Score: {}", score);
-    hexstr::print_bytes(string);
-    println!("{}", String::from_utf8(string.to_vec()).unwrap());
+    println!("{}", String::from_utf8(decoded).unwrap());
 }
